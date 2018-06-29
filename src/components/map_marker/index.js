236d767ab617ca2347c752e3983298adcd30396f
minagawah <minagawa@tokyo800.jp>
2018-06-29 13:35:12 +0900

    Fixed a bug on marker.

--- src/components/map_marker/index.js
+++ src/components/map_marker/index.js
@@ -2,20 +2,21 @@
 import template from './template.html';
 
 export default {
-  name: 'marker',
+  name: 'map-marker',
   template,
   props: {
     google: Object, // Provided by "components/google_map_loader".
     map: Object, // Provided by "components/google_map_loader".
-    spot: Object, // Given directly from "views/map".
+    marker: Object, // Given directly from "views/map".
   },
   data() {
-    return { marker: null };
+    return { spot: null };
   },
   mounted() {
+    const { map } = this;
     const { Marker } = this.google.maps;
-    const { name: title, lat, lng } = this.spot;
+    const { name: title, lat, lng } = this.marker || {};
     const position = { lat, lng };
-    this.marker = new Marker({ title, map: this.map, position });
+    this.spot = new Marker({ title, map, position });
   },
 };
